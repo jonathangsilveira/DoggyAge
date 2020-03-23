c9f6783f2a0f6c950f040c8b7bd4ea7a8c2f2c20
Jonathan Giorgi Silveira <jonathan.silveira-ext@ab-inbev.com>
2020-03-22 21:25:42 -0300

    First app

--- DoggyAge/ViewController.swift
+++ DoggyAge/ViewController.swift
@@ -9,12 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //MARK: Properties
+    @IBOutlet weak var ageField: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    //MARK: Actions
+    @IBAction func onDiscoverAgePressed(_ sender: UIButton) {
+        let text = ageField.text
+        if text?.isEmpty == true {
+            resultLabel.isHidden = true
+            return
+        }
+        resultLabel.isHidden = false
+        let dogAge = Int(text!)!
+        let humanAge = dogAge * 7
+        resultLabel.text = "Your doggy's human age is \(humanAge)"
+    }
+    
+    
 }
 
